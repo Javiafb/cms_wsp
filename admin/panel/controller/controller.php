e6abf8c36ef3769cf9bb2361bf7daf7b30e31058
Javiafb <jfernandezb02@gmail.com>
2024-11-21 20:21:07 -0500

    panel de administracion del cms

---
+++ admin/panel/controller/controller.php
@@ -0,0 +1,12 @@
+<?php 
+
+class vistas{
+
+    public function index(){
+        require_once '../panel/views/index.php';
+    }
+}
+
+
+
+?>
---
+++ admin/panel/controller/controller_rutas.php
@@ -0,0 +1,52 @@
+
+
+<?php
+
+$rut = array();
+
+if (isset($_GET['rut'])) {
+    $rut = explode("/", $_GET['rut']);
+}
+
+// Si no hay rut, redirigir a index
+if (empty($rut[0])) {
+    require_once "../panel/views/index.php";
+    exit();
+}
+
+// Manejar rutas específicas
+switch ($rut[0]) {
+    case "index":
+        require_once "../panel/views/index.php";
+        exit();
+    case "ui-buttons":
+        require_once "../panel/views/ui-buttons.php";
+        exit();
+    case "ui-alerts":
+        require_once "../panel/views/ui-alerts.php";
+        exit();
+    case "ui-card":
+        require_once "../panel/views/ui-card.php";
+        exit();
+    case "ui-forms":
+        require_once "../panel/views/ui-forms.php";
+        exit();
+    case "ui-typography":
+        require_once "../panel/views/ui-typography.php";
+        exit();
+    case "login":
+        require_once "../panel/views/authentication-login.php";
+        exit();
+    case "register":
+        require_once "../panel/views/authentication-register.php";
+        exit();
+    case "icon-tabler":
+        require_once "../panel/views/ui-tabler.php";
+        exit();
+    case "sample-page":
+        require_once "../panel/views/sample-page.php";
+        exit();
+    default:
+        echo 'Error: Página no encontrada';
+        exit();
+}
